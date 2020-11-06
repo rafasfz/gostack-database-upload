cca9f8ed054e5dfdddc7c1dd445144d2f8319c93
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2020-11-06 19:35:41 -0300

    Created route and service for create a transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
+import { getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    const checkCategoryExist = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExist) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+    }
+
+    const transactionCategory = (await categoriesRepository.findOne({
+      where: { title: category },
+    })) as Category;
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
