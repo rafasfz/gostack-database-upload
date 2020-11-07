566d15b26b6cef7983f8e552f40ec86f4beb4ad8
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2020-11-06 23:05:38 -0300

    Created route for import file csv for create transactions

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,40 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filename: string): Promise<Transaction[]> {
+    const csvFilePath = path.resolve(__dirname, '..', '..', 'tmp', filename);
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const transactions: Transaction[] = [];
+
+    parseCSV.on('data', async line => {
+      const createTransaction = new CreateTransactionService();
+
+      const transaction = await createTransaction.execute({
+        title: line[0],
+        type: line[1],
+        value: Number(line[2]),
+        category: line[3],
+      });
+      transactions.push(transaction);
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    return transactions;
   }
 }
 
